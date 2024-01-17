d2eda3052b6ce96bb77bd90db0e4799fd6ebec10
stevendouglasyagel <155750165+stevendouglasyagel@users.noreply.github.com>
2024-01-17 16:07:48 +0000

    Initial commit

---
+++ node_modules/react-microsoft-login-advanced/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var MicrosoftLogin_1 = require("./MicrosoftLogin");
+exports.MicrosoftLogin = MicrosoftLogin_1.default;
+exports.default = MicrosoftLogin_1.default;
+//# sourceMappingURL=index.js.map
